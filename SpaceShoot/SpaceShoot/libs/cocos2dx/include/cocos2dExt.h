4bd90af2d60a016adcc8b6a659d2953d03030b5a
zhangjie <304580317@qq.com>
2012-07-24 14:56:09 +0800

    1

--- SpaceShoot/SpaceShoot/libs/cocos2dx/include/cocos2dExt.h
+++
@@ -1,9 +0,0 @@
-#ifndef __COCOS2DEXT_H__
-#define __COCOS2DEXT_H__
-
-#include "extensions/CCNotificationCenter/CCNotificationCenter.h"
-#include "extensions/CCControlExtension/CCControlExtensions.h"
-#include "extensions/CCListView/CCListView.h"
-#include "extensions/CCTextureWatcher/CCTextureWatcher.h"
-
-#endif /* __COCOS2DEXT_H__ */
